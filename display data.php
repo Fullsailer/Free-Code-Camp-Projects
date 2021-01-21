4491ff1c3cbe3f4586f94b22d19280bb0aa1c84d
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2021-01-21 03:23:07 -0500

    Connecting DB to Website

---
+++ display data.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>Display Data into DB</title>
+<style type="text/css">
+table {
+    border: 2px solid red;
+    background-color: #FFC;
+    }
+
+th {
+    border-bottom: 5px solid #000;
+    }
+
+td {
+    border-bottom: 2px solid #666;
+    }
+</style>
+</head>
+<body>
+
+<h1>Display Date from DB</h1>
+
+<?php
+include('connect-mysql.php');
+
+$sqlget = "SELECT * FROM people";
+$sqldata = mysqli_query($dbcon, $sqlget) or die('error getting');
+
+echo "<table>";
+echo "<tr><th>ID</th><th>First Name</th><tj>Last Name</th><th>";
+
+while($row = mysqli_fetch_array($sqldata, MYSQLI_ASSOC)) {
+    echo "<tr><td>";
+    echo $row['peopleid'];
+    echo "</td><td>";
+    echo $row['firstname'];
+    echo "</td><td>";
+    echo $row['lastname'];
+    echo "</td></tr>";
+    }
+
+echo "</table>";
+
+?>
+
+</body>
+</html>
